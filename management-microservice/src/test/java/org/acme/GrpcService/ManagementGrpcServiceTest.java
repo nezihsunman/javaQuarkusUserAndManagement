fe89ce98088ed34f288027db04e1bb6b9a999488
DESKTOP-RM640OC\nezih <nezih.sunman@ozu.edu.tr>
2022-03-13 21:53:13 +0300

    not finish management test

--- management-microservice/src/test/java/org/acme/GrpcService/ManagementGrpcServiceTest.java
+++ management-microservice/src/test/java/org/acme/GrpcService/ManagementGrpcServiceTest.java
@@ -2,13 +2,17 @@ package org.acme.GrpcService;
 
 import io.quarkus.grpc.GrpcClient;
 import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Uni;
 import org.acme.*;
+import org.acme.DTOs.UserListDto;
 import org.acme.entity.Dashboard;
 import org.acme.enums.Approved;
 import org.acme.enums.DisabledOrEnabled;
 import org.acme.kafka.KafkaConsumer;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.mockito.InjectMock;
 import org.mockito.Mockito;
@@ -16,17 +20,23 @@ import org.mockito.Mockito;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @QuarkusTest
 class ManagementGrpcServiceTest {
 
     ManagementGrpcService managementGrpcService;
 
+    @InjectMock
+    Session session;
+    @InjectMock
+    KafkaConsumer kafkaConsumer;
 
     @Test
     void getListOfUser() {
@@ -59,10 +69,71 @@ class ManagementGrpcServiceTest {
 
     @Test
     void approveListOfUser() {
+        PanacheMock.mock(Dashboard.class);
+
+        Dashboard dashboard = new Dashboard();
+        dashboard.setUsername("user");
+        dashboard.setUserId(1L);
+        dashboard.setApproved(Approved.NOT_APPROVED);
+        dashboard.setDisabledOrEnabled(DisabledOrEnabled.DISABLED);
+
+        Mockito.when(Dashboard.findById(Mockito.anyLong())).thenReturn(dashboard);
+
+        Query mockQuery = Mockito.mock(Query.class);
+        Mockito.doNothing().when(kafkaConsumer).approveUsers(any());
+        Mockito.doNothing().when(session).persist(any());
+
+        List<Dashboard> dashboardList = new ArrayList<>();
+        for (int i = 10; i < 20 ; i++) {
+            Dashboard dashboard2 = new Dashboard();
+            dashboard2.setUsername("mock_user" + i);
+            dashboard2.setUserId((long) i);
+            dashboard2.setApproved(Approved.APPROVED);
+            dashboard2.setDisabledOrEnabled(DisabledOrEnabled.ENABLED);
+            dashboardList.add(dashboard2);
+        }
+
+        Mockito.when(Dashboard.getAllList()).thenReturn(dashboardList);
+
+        CompletableFuture<String> message = new CompletableFuture<>();
+        var req = ApproveOrDisapproveUserDto.newBuilder().setUserId(1).setApprovedOrNot(String.valueOf(Approved.APPROVED)).build();
+        this.managementGrpcService = new ManagementGrpcService();
+        managementGrpcService.approveListOfUser(req)
+                .subscribe().with(reply -> message.complete(reply.getResponseList().toString()));
+
     }
 
     @Test
     void enableOrDisableUser() {
+        PanacheMock.mock(Dashboard.class);
+
+        Dashboard dashboard = new Dashboard();
+        dashboard.setUsername("user");
+        dashboard.setUserId(1L);
+        dashboard.setApproved(Approved.NOT_APPROVED);
+        dashboard.setDisabledOrEnabled(DisabledOrEnabled.DISABLED);
+
+
+        Mockito.when(Dashboard.findById(Mockito.anyLong())).thenReturn(dashboard);
+
+
+        List<Dashboard> dashboardList = new ArrayList<>();
+        for (int i = 10; i < 20 ; i++) {
+            Dashboard dashboard2 = new Dashboard();
+            dashboard2.setUsername("mock_user" + i);
+            dashboard2.setUserId((long) i);
+            dashboard2.setApproved(Approved.APPROVED);
+            dashboard2.setDisabledOrEnabled(DisabledOrEnabled.ENABLED);
+            dashboardList.add(dashboard2);
+        }
+
+        Mockito.when(Dashboard.getAllList()).thenReturn(dashboardList);
+
+        CompletableFuture<String> message = new CompletableFuture<>();
+        var req = EnableOrDisableDto.newBuilder().setUserId(1).setDisabledOrEnabled(String.valueOf(DisabledOrEnabled.ENABLED)).build();
+        this.managementGrpcService = new ManagementGrpcService();
+        managementGrpcService.enableOrDisableUser(req)
+                .subscribe().with(reply -> message.complete(reply.getResponseList().toString()));
     }
 
 }
